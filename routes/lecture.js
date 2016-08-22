485bbacb3a0e81bbc65799e43b54e3f7e96486a5
dw <danwoopark@gmail.com>
2016-08-22 16:39:21 +0900

    JADE has been removed

--- routes/lecture.js
+++ routes/lecture.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('lecture');
+	res.sendFile(path.join(__dirname, '../views', 'lecture.html'));
 });
 
 module.exports = router;
--- routes/question.js
+++ routes/question.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('question');
+  res.sendFile(path.join(__dirname, '../views', 'question.html'));
 });
 
 module.exports = router;
