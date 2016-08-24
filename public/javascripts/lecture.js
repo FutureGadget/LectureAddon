a84089ca48087f758478db98a97dc6475840574d
dw <danwoopark@gmail.com>
2016-08-24 19:50:24 +0900

    jquery mobile SPA수정중

--- public/javascripts/lecture.js
+++ public/javascripts/lecture.js
@@ -35,7 +35,7 @@ function addQuestion(data, removable) {
 
  Remove the "취소하기" button when clicked and close the input form.
  */
-$("#qbutton").on("click", function() {
+function questionButtonClicked() {
     $("#qform").slideToggle();
     if ($("#qform").attr("class")=='hidden') {
         changeButtonIcon($("#qbutton"), 'delete', 'myIcon', '취소')
@@ -74,7 +74,47 @@ $("#qbutton").on("click", function() {
         $("#qform").attr("class","hidden");
         $("#sbutton").remove();
     }
-});
+};
+
+function commentButtonClicked() {
+    $("#qform").slideToggle();
+    if ($("#qform").attr("class")=='hidden') {
+        changeButtonIcon($("#qbutton"), 'delete', 'myIcon', '취소')
+            .attr("id", "cbutton")
+            .css("width", "44%");
+        $("#qform").attr("class","visible");
+
+        var button = $("#cbutton").clone();
+        changeButtonIcon(button, 'check', 'delete', '제출')
+            .attr("id", "sbutton")
+        $("#cbutton").before(button);
+        button.on("click",function(){
+            var qdata = {
+                title: $('#qtitle').val(),
+                content: $('.textEditor').val()
+            };
+            $.ajax({
+                url: 'http://localhost:3000/comment',
+                dataType: 'jsonp',
+                jsonp: 'callback',
+                type: 'GET',
+                data: qdata,
+                success: function(data) { // data returned from the server (dao). The data must contain id field.
+                    
+                }
+            });
+            $('#qtitle').val('');
+            $('.textEditor').jqteVal('');
+            $('#cbutton').click();
+        });
+    } else {
+        changeButtonIcon($("#cbutton"), 'myIcon', 'delete', '댓글달기')
+            .attr("id", "qbutton")
+            .css("width", "100%");
+        $("#qform").attr("class","hidden");
+        $("#sbutton").remove();
+    }
+};
 
 /*
  Change the button's text and icon.
